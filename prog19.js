974afd0e00c36ec198789a2599611d082f432252
FabricioVasconcelosB <fabricio-bio2023@hotmail.com>
2024-02-01 08:28:55 -0300

    Atualização

---
+++ prog19.js
@@ -0,0 +1,13 @@
+function main(){
+    var entrada=Number(document.getElementById('entrada').value);
+    escreverAsteristico(entrada)
+}
+function escreverAsteristico(tamanho){ 
+    for(var i=1;i<=tamanho;i++){
+        var linha='';
+        for(var j=1;j<=i;j++){
+            linha+='*';
+        }
+        console.log(linha)
+    }
+}
